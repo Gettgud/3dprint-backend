320d72a3c8a2336815a81739e3c8eb9b16e42ca0
Carlos German <carlosgerman353@gmail.com>
2022-09-01 21:23:32 -0400

    post item information to db

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://majfielwtbzupo:678b932034546ee4c7d8906512704b8f00df3e311ee650d4355ae0da02656a21@ec2-44-199-143-43.compute-1.amazonaws.com:5432/dcm34gl41nh898'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 	
 db = SQLAlchemy(app)
 
@@ -17,10 +17,10 @@ class Item(db.Model):
 	_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 	name = db.Column(db.Text(), nullable=False)
 	description = db.Column(db.Text(), nullable=False)
-	price = db.Column(db.Numeric(5,2), nullable=False)
+	price = db.Column(db.Numeric(6,2), nullable=False)
 
 	# A constructor function where we will pass the id, description and price and add as a new entry in the table.
-	def __init__(self, id, name, description, price):
+	def __init__(self, name, description, price):
 		self.name = name
 		self.description = description
 		self.price = price
@@ -42,15 +42,16 @@ def index():
 	if request.method == 'POST': # When a user clicks submit button it will come here.
 		data = request.form # request the data from the form in index.html fileee
 		name = data["name"]
-		email = data["email"]
+		price = data["price"]
+		description = data["description"]
 
-		new_data = User(name, email)
+		new_data = Item(name, description, price)
 		db.session.add(new_data)
 		db.session.commit()
 
-		user_data = User.query.all()
+		item_data = Item.query.all()
 
-		return render_template("index.html", user_data = user_data) # passes user_data variable into the index.html file.
+		return render_template("index.html", item_data = item_data) # passes user_data variable into the index.html file.
 
 	return render_template("index.html")
 	
